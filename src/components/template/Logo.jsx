4fe7faa81053893d47ba39fcb5bd514ffa1b06ba
Cecília Clarice <clarity.clarice@gmail.com>
2019-10-15 12:00:32 -0300

    Adicionando logo do porto

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -2,8 +2,11 @@ import './Logo.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Logo from '../../Assets/images/Logo.png'
+
 export default props =>
     <aside className="logo">
         <Link to="/" className="logo">
+            <img src={Logo} alt="Porto de Itajaí" />
         </Link>
     </aside>

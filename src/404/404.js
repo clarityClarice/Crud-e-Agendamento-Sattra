847b80edbedc9e722ff41b5522895d52a59764ec
Cecília Clarice <clarity.clarice@gmail.com>
2019-10-15 14:59:37 -0300

    Acrestentando pagina 404

---
+++ src/404/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Quatrocentos extends React.Component{
+    render(){
+        return(
+            <div className="content">
+                <span>404</span>
+            </div>
+        )
+    }
+}
+
+export default Quatrocentos

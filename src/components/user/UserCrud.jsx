e04bc92667f0e2a72ad2a6bd99f46628a28e725b
Cecília Clarice <clarity.clarice@gmail.com>
2019-11-05 14:59:43 -0300

    Concertando alguns bugs do cadastro de novo usuário

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -8,17 +8,21 @@ import Axios from 'axios'
 
 export default class UserCrud extends Component {
     state = {
-        pessoa: {id: '', nome: '', data_nascimento: new Date(), email: '', telefone_principal: '', cpf: '', passaporte:'',
-                cnh:'', validade_cnh: new Date(), cargo: '', observacao: '', responsavel_id: ''},
-        setores: [{id: 0, nome: ''}],
-        setor: {id: '', nome: ''}
+        pessoa: {id: '', nome: '', data_nascimento: '01-01-2020', email: '', telefone_principal: '', cpf: '', passaporte:'',
+                cnh:'', validade_cnh: '01-01-2020', cargo: '', observacao: '', responsavel_id: 1},
+        setores: [{id: 0, nome: ''}]
     }
 
     async componentDidMount(){
         const response = await api.get('/ws/setores')
         this.setState({setores: response.data})
-        console.log(response.data)
-
+        this.setState({
+            setor: this.state.setores[0],
+            pessoa: {
+                ...this.state.pessoa,
+                responsavel_id: this.state.setores[0].id
+            }
+        })
         var urlArray = window.location.href.split('/')
         const idFromUrl = urlArray[6]? urlArray[6] : 0
         if(idFromUrl != 0){
@@ -28,7 +32,6 @@ export default class UserCrud extends Component {
 
            const response = await api.get(`/ws/pessoa/${idFromUrl}`)
            this.setState({pessoa: response.data})
-           console.log(this.state.pessoa)
 
             this.setState({ setor: {
                 id: response.data.responsavel_id
@@ -57,13 +60,12 @@ export default class UserCrud extends Component {
 
     save(){
         const method = this.state.pessoa.id ? 'put' : 'post'
-        console.log(this.state.pessoa)
-        console.log(method)
         Axios[method](`http://192.168.25.13:8089/ws/pessoa/${this.state.pessoa.id}`, this.state.pessoa)
             .then(resp => {
                 this.setState({ pessoa: this.state.pessoa})
-                console.log(resp)
             })
+        
+            window.location.href = `/#/app/cadastros`
     }
 
 
@@ -75,10 +77,13 @@ export default class UserCrud extends Component {
     }
 
     updateSectorField(event){
+        const pessoa = {...this.state.pessoa}
+        pessoa[event.target.name] = event.target.value.split(' ')[0]
+
         this.setState({
             pessoa: {
                 ...this.state.pessoa,
-                responsavel_id: event.target.value.split(' ')[0]
+                responsavel_id: pessoa.responsavel_id
             }
         })
     }
@@ -92,7 +97,7 @@ export default class UserCrud extends Component {
                         <div className="form-group">
                             <label> Nome </label>
                             <input type="text"
-                                name="name"
+                                name="nome"
                                 className="form-control"
                                 value={this.state.pessoa.nome}
                                 onChange={e => this.updateField(e)}
@@ -108,7 +113,7 @@ export default class UserCrud extends Component {
                                 className="form-control"
                                 value={this.state.pessoa.data_nascimento}
                                 onChange={e => this.updateField(e)}
-                                placeholder=" dd/mm/aaaa"
+                                placeholder=" dd-mm-aaaa"
                             />
                         </div>
                     </div>
@@ -153,7 +158,7 @@ export default class UserCrud extends Component {
                                 className="form-control"
                                 value={this.state.pessoa.validade_cnh}
                                 onChange={e => this.updateField(e)}
-                                placeholder=" dd/mm/aaaa"
+                                placeholder=" dd-mm-aaaa"
                             />
                         </div>
                     </div>
@@ -183,7 +188,7 @@ export default class UserCrud extends Component {
                         <div className="form-group" >
                             <label> Cargo </label>
                             <input type="text"
-                                name="Cargo"
+                                name="cargo"
                                 className="form-control"
                                 value={this.state.pessoa.cargo}
                                 onChange={e => this.updateField(e)}
@@ -194,7 +199,7 @@ export default class UserCrud extends Component {
                         <div className="form-group" >
                             <label> Observações </label>
                             <input type="text"
-                                name="observacoes"
+                                name="observacao"
                                 className="form-control"
                                 value={this.state.pessoa.observacao}
                                 onChange={e => this.updateField(e)}
@@ -204,10 +209,13 @@ export default class UserCrud extends Component {
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label> Setor Responsável</label>
-                            <select onChange={ e=> this.updateSectorField(e)} value={this.state.setor.nome}>
-                                {this.state.setores.map (setor => <option key={setor.id}> {setor.id} {setor.nome} </option>)}
+                            <select name="responsavel_id" onChange={ e=> this.updateSectorField(e)} value={this.state.pessoa.responsavel_id}>
+                                {this.state.setores.map (setor => <option key={setor.id}>{setor.id}</option>)}
                             </select>
                         </div>
+                        <ul>
+                            {this.state.setores.map (setor => <li key={setor.id}>{setor.id} -> {setor.nome}</li>)}
+                        </ul>
                     </div>
                 </div>
                 <div className="row">

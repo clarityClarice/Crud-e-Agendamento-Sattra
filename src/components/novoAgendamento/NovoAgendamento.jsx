abeb9e2d915b0f35720a1778073fd71c6b7ce2f6
Cecília Clarice <clarity.clarice@gmail.com>
2019-11-05 14:59:21 -0300

    Iniciando pagina novo agendamento

--- src/components/novoAgendamento/NovoAgendamento.jsx
+++ src/components/novoAgendamento/NovoAgendamento.jsx
@@ -4,10 +4,26 @@ import Main from '../template/Main'
 import './NovoAgendamento.css'
 
 class NovoAgendamento extends React.Component{
+    state = {
+        pessoa: '',
+        data_inicial: '',
+        data_final: '',
+        pessoa_fisica_id: 0,
+        observacao: '',
+        setor_responsavel: 1
+    }
+
+    
     render(){
         return(
+            
             <Main icon="calendar" title="Agenda" subtitle="Novo Agendamento">
-
+                {/* "data_inicial":"A Escolher formato",
+                    "data_final":"A Escolher formato",
+                    "pessoa_fisica_id": 1,
+                    "observacao": "Obs...",
+                    "setor_responsavel": 2*/
+                }
             </Main>
         )
     }
